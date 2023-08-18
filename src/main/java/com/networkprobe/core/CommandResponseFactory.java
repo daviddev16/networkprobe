eb3b5a427f7aebfdb7ea10d8686c854a15592d0c
David Duarte <37241920+daviddev16@users.noreply.github.com>
2023-08-18 00:10:55 -0300

    added diagram

---
+++ src/main/java/com/networkprobe/core/CommandResponseFactory.java
@@ -0,0 +1,38 @@
+package com.networkprobe.core;
+
+import com.networkprobe.core.annotation.Singleton;
+import com.networkprobe.core.api.ResponseEntity;
+import com.networkprobe.core.api.ResponseEntityFactory;
+import com.networkprobe.core.caching.ProcessedResponseEntity;
+import com.networkprobe.core.caching.StaticResponseEntity;
+import com.networkprobe.core.experimental.Simplexer;
+
+/**
+ *  CommandResponseFactory é uma implementação do ResponseEntityFactory, que  é responsável
+ *  por determinar qual será a o tipo de ResponseEntity conforme o conteúdo do comando informado
+ *  no arquivo de template JSON. O CommandResponseFactory faz a verificação se há expressão processável
+ *  dentro do comando e faz decisão se o comando vai ser do tipo processado ou um conteúdo estático
+ *  como uma String, por exemplo.
+ **/
+
+@Singleton(creationType = SingletonType.DYNAMIC, order = 10)
+public final class CommandResponseFactory implements ResponseEntityFactory {
+
+    public CommandResponseFactory()
+    {
+        SingletonDirectory.denyInstantiation(this);
+    }
+
+    public ResponseEntity<?> responseEntityOf(String rawContent, boolean cachedOnce)
+    {
+        if (Simplexer.containsExpression(rawContent))
+            return new ProcessedResponseEntity(rawContent, cachedOnce);
+        else
+            return new StaticResponseEntity(rawContent);
+    }
+
+    public static CommandResponseFactory getFactory() {
+        return SingletonDirectory.getSingleOf(CommandResponseFactory.class);
+    }
+
+}
---
+++ src/main/java/com/networkprobe/core/caching/StaticResponseEntity.java
@@ -0,0 +1,25 @@
+package com.networkprobe.core.caching;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * O valor de um StaticResponseEntity não muda durante o tempo de
+ * execução e sempre retornará um objeto do tipo String.
+ *  */
+public class StaticResponseEntity extends DefaultResponseEntity<String> {
+
+    public StaticResponseEntity(String rawContent) {
+        super(rawContent);
+    }
+
+    @Override
+    public @NotNull String getContent() {
+        return getRawContent() != null ? getRawContent() : "";
+    }
+
+    @Override
+    public boolean isCachedOnce() {
+        return true;
+    }
+
+}
